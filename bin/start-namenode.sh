325f47344b152f54f2c101698fa668d9abb80364
Michael Reynolds <reynoldsm88@gmail.com>
2017-12-19 08:26:23 -0500

    remove -net=bridge from the hadoop scripts

--- bin/start-namenode.sh
+++ bin/start-namenode.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -h hdfs.namenode --name hdfs-namenode -p 50070:50070 -p 9000:9000 -t --net=bridge reynoldsm88/hdfs-namenode
+docker run -h hdfs.namenode --name hdfs-namenode -p 50070:50070 -p 9000:9000 -t reynoldsm88/hdfs-namenode
