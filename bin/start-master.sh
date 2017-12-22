550979fe7e08d7df16693e6a7a585178d6864d3e
michael <michael@UCO1L-PAMOR01.localdomain>
2017-12-22 13:18:44 -0500

    getting spark jobs to work on linux

--- bin/start-master.sh
+++ bin/start-master.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v /Users/michael/tools/data/:/etc/spark/apps --name spark-master -p 8080:8080 -p 7077:7077 -p 6066:6066 -t reynoldsm88/spark-master
+docker run -v /home/michael/tools/data:/etc/spark/apps --name spark-master -p 8080:8080 -p 7077:7077 -p 6066:6066 -t reynoldsm88/spark-master
--- bin/start-worker.sh
+++ bin/start-worker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -v /Users/michael/tools/data/:/etc/spark/apps --name spark-worker -p 8081:8081 -p 7078:7078 -t reynoldsm88/spark-worker
+docker run -v /home/michael/tools/data:/etc/spark/apps --name spark-worker -p 8081:8081 -p 7078:7078 -t reynoldsm88/spark-worker
